39e63f07df798d04c144cd6457112b204181648e
Pedro Tomás <1pedrotomas1@gmail.com>
2022-10-09 21:51:49 +0100

    Add main elements to App.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Ground } from './components/Ground';
+import { Player } from './components/Player';
+import { FPV } from './components/FPV';
+import { Cubes } from './components/Cubes';
+
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
+      
       <Canvas>
+        <Sky sunPosition={[100, 100, 20]}></Sky>
+        <ambientLight intensity={0.5}></ambientLight>
+        <FPV />
+        <Physics>
+          <Player /> 
+          <Cubes />
+          <Ground  />
+        </Physics>
       </Canvas>
+      <div className='absolute centered cursor'>+</div>
     </>
   );
 }
