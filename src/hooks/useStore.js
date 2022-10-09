ca4d0396cb4268bcfc6f564d75557434e4baa17e
Pedro Tomás <1pedrotomas1@gmail.com>
2022-10-09 21:52:54 +0100

    Create hook to manage game state.

---
+++ src/hooks/useStore.js
@@ -0,0 +1,32 @@
+import create from "zustand";
+import { nanoid } from "nanoid";
+
+export const useStore = create((set) => ({
+    texture: "dirt",
+    cubes: [{
+        key: nanoid(),
+        pos: [1, 0.5, 1],
+        texture: "dirt"
+    }, {
+        key: nanoid(),
+        pos: [2, 0.5, 1],
+        texture: "wood"
+    }],
+    addCube: (x, y, z) => {
+        set((prev => ({
+            cubes: [
+                ...prev.cubes,
+                {
+                    key: nanoid(),
+                    pos: [x, y, z],
+                    texture: prev.texture
+                }
+            ]
+        })))
+    },
+    removeCube: () => {},
+    setTexture: () => {},
+    saveWorld: () => {},
+    resetWorld: () => {},
+
+}))
